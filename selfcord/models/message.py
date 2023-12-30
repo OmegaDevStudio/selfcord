47e338f0741be79404b9bce8f21dda05895d8d12
Shell1010 <amin.dev03@gmail.com>
2023-12-30 01:53:46 +0000

    Added Embed Object, kick/ban

--- selfcord/models/message.py
+++ selfcord/models/message.py
@@ -73,6 +73,7 @@ class MessageAck:
     def __init__(self, payload: dict, bot) -> None:
         self.bot = bot
         self.http = bot.http
+        self.update(payload)
 
     def update(self, payload: dict):
         self.channel_id: str = payload['channel_id']
@@ -83,21 +84,124 @@ class MessageAck:
         self.message = self.bot.fetch_message(self.message_id)
         self.version = payload['version']
 
+class EmbedField:
+    def __init__(self, payload: dict, bot):
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.name = payload.get("name")
+        self.value = payload.get("value")
+        self.inline = payload.get("inline")
+
+class EmbedThumbnail:
+    def __init__(self, payload: dict, bot):
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.url = payload['url']
+        self.proxy_url = payload.get("proxy_url")
+        self.height = payload.get("height")
+        self.width = payload.get("width")
+
+class EmbedVideo:
+    def __init__(self, payload: dict, bot):
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.url = payload['url']
+        self.proxy_url = payload.get("proxy_url")
+        self.height = payload.get("height")
+        self.width = payload.get("width")
+
+class EmbedImage:
+    def __init__(self, payload: dict, bot):
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.url = payload['url']
+        self.proxy_url = payload.get("proxy_url")
+        self.height = payload.get("height")
+        self.width = payload.get("width")
+
+class EmbedProvider:
+    def __init__(self, payload: dict, bot):
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.name = payload.get("name")
+        self.url = payload.get("url")
+
+class EmbedAuthor:
+    def __init__(self, payload: dict, bot) -> None:
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+    
+    def update(self, payload: dict):
+        self.name = payload.get("name")
+        self.url = payload.get("url")
+        self.icon_url = payload.get("icon_url")
+        self.proxy_icon_url = payload.get("proxy_icon_url")
+
+class EmbedFooter:
+    def __init__(self, payload: dict, bot) -> None:
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.text = payload.get("text")
+        self.icon_url = payload.get("icon_url")
+        self.proxy_icon_url = payload.get("proxy_icon_url")
+
+
+class Embed:
+    def __init__(self, payload: dict, bot):
+        self.bot = bot
+        self.http = bot.http
+        self.update(payload)
+
+    def update(self, payload: dict):
+        self.fields: list[EmbedField] = [EmbedField(field, self.bot) for field in payload.get("fields", [])]
+        self.title: Optional[str] = payload.get("title")
+        self.description: Optional[str] = payload.get("description")
+        self.type: Optional[str] = payload.get("type")
+        self.url: Optional[str] = payload.get("url")
+        self.timestamp: Optional[int] = payload.get("timestamp")
+        self.color: Optional[int] = payload.get("color")
+        self.footer: Optional[str] = payload.get("footer")
+        self.image: Optional[dict] = payload.get("image")
+        self.thumbnail: Optional[dict] = payload.get("thumbnail")
+        self.video: Optional[dict] = payload.get("video")
+        self.provider: Optional[dict] = payload.get("provider")
+        self.author: Optional[str] = payload.get("author")
+
+
 class MessageReactionAdd:
     def __init__(self, payload: dict, bot) -> None:
         self.bot = bot
         self.http = bot.http
+        self.update(payload)
 
     def update(self, payload: dict):
-        self.burst: bool = payload['burst']
-        self.channel_id: str = payload['channel_id']
-        self.emoji: str = payload['emoji']
+        self.burst: bool = payload.get("burst", False)
+        self.channel_id: Optional[str] = payload.get("channel_id")
+        self.emoji: Optional[str] = payload.get("emoji")
         self.guild_id: Optional[str] = payload.get("guild_id")
-        self.type = payload.get("type", 0)
-        self.user_id = payload['user_id']
-        self.message = self.bot.fetch_message(payload['message_id'])
-        self.message_id = payload['message_id']
-        self.author = self.bot.fetch_user(payload['message_author_id'])
-        self.author_id = payload['message_author_id']
-        self.user_id = payload['user_id']
+        self.type: int = payload.get("type", 0)
+        self.user_id: Optional[str] = payload.get("user_id")
+        self.message_id: str = payload.get("message_id", "")
+        self.message: Message = self.bot.fetch_message(self.message_id)
+        self.author_id: Optional[str] = payload.get("message_author_id")
+        self.author = self.bot.fetch_user(self.author_id)
         self.user = self.bot.fetch_user(self.user_id) 
